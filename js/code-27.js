0ae986e49015a147c2a42dcd1fc61d4e049a96a5
SongLin <songlin1026@gmail.com>
2021-06-07 11:10:45 +0800

    user-27

---
+++ js/code-27.js
@@ -0,0 +1,5 @@
+document.querySelector(".user-27").textContent="松霖";
+let user27=document.querySelector(".user-27")
+user27.setAttribute("onMouseOver","this.classList.add('Mouse')")
+user27.setAttribute("onMouseOut","this.classList.remove('Mouse')")
+
